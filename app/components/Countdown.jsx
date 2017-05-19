9d99c529e2aefb6c37e7488b001c2de3e5ef64e0
Worldofwbdesign <zavodzamutov@gmail.com>
2017-05-19 20:58:16 +0300

    Add Countdown tests

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -5,12 +5,35 @@ let React = require('react'),
 let Countdown = React.createClass({
 
   getInitialState() {
-    return { count: 0 };
+    return {
+      count: 0,
+      countdownStatus: 'stopped'
+    };
+  },
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+      }
+    }
+  },
+
+  startTimer() {
+    this.timer = setInterval( () => {
+      let newCount = this.state.count - 1;
+      this.setState({
+        count: newCount > 0 ? newCount : 0,
+      })
+    }, 1000)
   },
 
   handleSetCountdown(seconds) {
     this.setState({
-      count: seconds
+      count: seconds,
+      countdownStatus: 'started'
     })
   },
 
@@ -23,7 +46,7 @@ let Countdown = React.createClass({
         <CountdownForm onSetCountdown={this.handleSetCountdown}/>
       </div>
     )
-  }
+  },
 
 });
 
---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,42 @@
+let React = require('react');
+let ReactDOM = require('react-dom');
+let expect = require('expect');
+let TestUtils = require('react-addons-test-utils');
+let $ = require('jQuery');
+
+let Countdown = require('Countdown');
+
+describe('Countdown', () => {
+
+  it('Should exist', () => {
+    expect(Countdown).toExist();
+  });
+
+  describe('handleSetCountdown', () => {
+
+    it('should update countdownStatus and run timer', (done) => {
+      let countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(10);
+
+      expect(countdown.state.count).toBe(10);
+      expect(countdown.state.countdownStatus).toBe('started');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(9);
+        done();
+      }, 1001);
+    });
+
+    it('should stop on zero value', (done) => {
+      let countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(1);
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        done();
+      }, 3000);
+    })
+  })
+
+
+})
